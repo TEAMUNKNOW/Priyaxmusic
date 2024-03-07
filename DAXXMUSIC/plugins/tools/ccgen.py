ca14d1b93afc79c0f6607b0c85e2af407b2bfa99
TEAMUNKNOW <142662898+TEAMUNKNOW@users.noreply.github.com>
2024-03-07 09:23:06 +0530

    Update ccgen.py

--- DAXXMUSIC/plugins/tools/ccgen.py
+++ DAXXMUSIC/plugins/tools/ccgen.py
@@ -7,27 +7,27 @@ from pyrogram.types import *
 async def gen_cc(client, message):
     if len(message.command) < 2:
         return await message.reply_text(
-            "**Please Give Me a Bin To\nGenerate Cc ...**"
+            "**ᴘʟᴇᴀsᴇ ɢɪᴠᴇ ᴍᴇ ᴀ ʙɪɴ ᴛᴏ\nɢᴇɴᴇʀᴀᴛᴇ ᴄᴄ ...**"
         )
     try:
         await message.delete()
     except:
         pass
-    aux = await message.reply_text("**Generating ...**")
+    aux = await message.reply_text("**ɢᴇɴᴇʀᴀᴛɪɴɢ.....**")
     bin = message.text.split(None, 1)[1]
     if len(bin) < 6:
-        return await aux.edit("**❌ Wrong Bin❗...**")
+        return await aux.edit("**❌ ᴡʀᴏɴɢ ʙɪɴ❗...**")
     try:
         resp = await api.ccgen(bin, 10)
         cards = resp.liveCC
         await aux.edit(f"""
-**💠 Some Live Generated CC:**
+**💠 sᴏᴍᴇ ʟɪᴠᴇ ɢᴇɴᴇʀᴀᴛᴇᴅ ᴄᴄ:**
 `{cards[0]}`\n`{cards[1]}`\n`{cards[2]}`
 `{cards[3]}`\n`{cards[4]}`\n`{cards[5]}`
 `{cards[6]}`\n`{cards[7]}`\n`{cards[8]}`
 `{cards[9]}`
-**💳 Bin:** `{resp.results[0].bin}`
-**⏳ Time Took:** `{resp.took}`\n\n"""
+**💳 ʙɪɴ:** `{resp.results[0].bin}`
+**⏳ ᴛɪᴍᴇ ᴛᴏᴏᴋ:** `{resp.took}`\n\n"""
         )
     except Exception as e:
         return await aux.edit(f"**Error:** `{e}`")

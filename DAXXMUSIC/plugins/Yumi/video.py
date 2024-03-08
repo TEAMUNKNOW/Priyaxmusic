6d17f2620d250be591f7233f55ad3a1e3ae2d4ad
TEAMUNKNOW <142662898+TEAMUNKNOW@users.noreply.github.com>
2024-03-09 00:00:33 +0530

    Update video.py

--- DAXXMUSIC/plugins/Yumi/video.py
+++ DAXXMUSIC/plugins/Yumi/video.py
@@ -81,7 +81,7 @@ async def song_commad_private(client, message: Message, _):
     else:
         if len(message.command) < 2:
             return await message.reply_text(_["song_2"])
-    mystic = await message.reply_sticker(_["play_1"])
+    mystic = await message.reply_sticker(_["song_12"])
     query = message.text.split(None, 1)[1]
     try:
         (

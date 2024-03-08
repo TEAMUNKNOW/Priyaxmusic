fa86f891fabebf95a60f7e33e2981a9292434e7e
TEAMUNKNOW <142662898+TEAMUNKNOW@users.noreply.github.com>
2024-03-08 23:40:48 +0530

    Update totalmembers.py

--- DAXXMUSIC/plugins/admins/totalmembers.py
+++ DAXXMUSIC/plugins/admins/totalmembers.py
@@ -29,7 +29,7 @@ def user_command(client, message):
 
 
 # Command handler for /givelink command
-@app.on_message(filters.command("givelink"))
+@app.on_message(filters.command("givelink") & admin_filter)
 async def give_link_command(client, message):
     # Generate an invite link for the chat where the command is used
     chat = message.chat.id

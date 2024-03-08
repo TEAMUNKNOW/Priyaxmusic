9dd119e1334686bc69d61b4c1b2d0bb89c43d978
TEAMUNKNOW <142662898+TEAMUNKNOW@users.noreply.github.com>
2024-03-08 09:39:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/DAXXTEAM/DAXXMUSIC",
+    "https://github.com/TEAMUNKNOW/Priyaxmusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "Master")
 GIT_TOKEN = getenv(

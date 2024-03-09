1f0681e5a63845eb4d4b469ec80abfc5f0fc2868
TEAMUNKNOW <142662898+TEAMUNKNOW@users.noreply.github.com>
2024-03-09 15:01:00 +0530

    Update Gpt.py

--- DAXXMUSIC/plugins/tools/Gpt.py
+++ DAXXMUSIC/plugins/tools/Gpt.py
@@ -1,40 +1,26 @@
-import random
-import time
-import requests
+from OpsAi import Ai
+from asyncio import sleep as rest 
+from datetime import datetime 
 from DAXXMUSIC import app
-from config import BOT_USERNAME
-
-from pyrogram.enums import ChatAction, ParseMode
 from pyrogram import filters
 
-@app.on_message(filters.command(["chatgpt","ai","ask","gpt","solve"],  prefixes=["+", ".", "/", "-", "", "$","#","&"]))
-async def chat_gpt(bot, message):
-    try:
-        start_time = time.time()
-        await bot.send_chat_action(message.chat.id, ChatAction.TYPING)
 
-        if len(message.command) < 2:
-            await message.reply_text(
-                "ᴇxᴀᴍᴘʟᴇ:\n\n/chatgpt Where is TajMahal?"
-            )
-        else:
-            a = message.text.split(' ', 1)[1]
-            response = requests.get(f'https://chatgpt.apinepdev.workers.dev/?question={a}')
-
-            try:
-                # Check if "results" key is present in the JSON response
-                if "answer" in response.json():
-                    x = response.json()["answer"]
-                    end_time = time.time()
-                    telegram_ping = str(round((end_time - start_time) * 1000, 3)) + " ms"
-                    await message.reply_text(
-                        f" {x}      ᴀɴsᴡᴇʀɪɴɢ ʙʏ ➛  @ll_anime_dp_ll",
-                        parse_mode=ParseMode.MARKDOWN
-                    )
-                else:
-                    await message.reply_text("No 'results' key found in the response.")
-            except KeyError:
-                # Handle any other KeyError that might occur
-                await message.reply_text("Error accessing the response.")
-    except Exception as e:
-        await message.reply_text(f"**á´‡Ê€Ê€á´Ê€: {e} ")
+@app.on_message(filters.command("ask"))
+async def ai_bot(_, message):
+     if message.reply_to_message:
+      queri = message.reply_to_message.text
+      gonb = Ai(query=queri)
+      await message.reply(gonb.chat())
+     elif len(message.command) == 1:
+      return await message.reply("ʜᴇʟʟᴏ! ɪ'ᴍ ʏᴏᴜʀ ғʀɪᴇɴᴅʟʏ ᴀɪ ᴀssɪsᴛᴀɴᴛ. \nʜᴏᴡ ᴄᴀɴ ɪ ʜᴇʟᴘ ʏᴏᴜ ᴛᴏᴅᴀʏ?")
+     elif len(message.command) > 1:
+      queri = message.text.split(None,1)[1]
+     gonb = Ai(query=queri)
+     x = Ai(query=queri)
+     me = await message.reply_text("ᴘʀᴏᴄᴇssᴇs.....")
+     await rest(2)
+     mee = await me.edit_text("ᴀʟʟ ᴍᴏsᴛ ᴅᴏɴᴇ ʙᴀʙʏ....🖤")
+     await mee.delete()
+     await rest(1)
+     await message.reply(gonb.chat())
+     
